06361d92e8f7b475ab6ec464f645ebd9594925d0
20473BLESSY <92619472+20473BLESSY@users.noreply.github.com>
2021-10-30 22:12:55 +0530

    Create school.py

---
+++ school.py
@@ -0,0 +1,25 @@
+class Student():
+    # Constructor
+    def __init__(self, name, rollno, m1, m2):
+        self.name = name
+        self.rollno = rollno
+        self.m1 = m1
+        self.m2 = m2
+
+    # Function to create and append new student
+    def accept(self, Name, Rollno, marks1, marks2):
+        # use  ' int(input()) ' method to take input from user
+        ob = Student(Name, Rollno, marks1, marks2)
+        ls.append(ob)
+
+    # Function to display student details
+    def display(self, ob):
+        print("Name   : ", ob.name)
+        print("RollNo : ", ob.rollno)
+        print("Marks1 : ", ob.m1)
+        print("Marks2 : ", ob.m2)
+        print("\n")
+    # Constructor
+student_1 = Student()
+student_1 (blessy,473,25,25)
+print(student_1.name,student_1.rollno,student_1.marks1,student_1.marks2)
